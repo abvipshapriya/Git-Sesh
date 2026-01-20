1c5326c2ef7378c44b45e7ed8e314cbb714a3e6e
abvipshapriya <abvipshapriya@gmail.com>
2026-01-20 19:37:29 +0530

    Fix add function return type and clean output

--- B125003.c
+++ B125003.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
-int add(int a, int b){
-    int sum=a+b;
-    printf("sum is :%d",sum);
+#include <stdio.h>
+int add(int a, int b)
+{
+    int sum = a + b;
+    return sum;
 }
-int main(){
-    int a,b;
-   printf("enter two numbers");
-   scanf("%d %d",&a,&b); 
-   add(a,b);
+int main()
+{
+    int a, b;
+    printf("enter two numbers");
+    scanf("%d %d", &a, &b);
+    int result = add(a, b);
+    printf("sum is :%d", result);
     return 0;
 }
